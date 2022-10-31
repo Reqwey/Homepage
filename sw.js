54aed77964fbec9ff92025b7d866f450e229c045
Reqwey <50829219+Reqwey@users.noreply.github.com>
2022-10-31 20:59:18 +0800

    round-circle

--- sw.js
+++ sw.js
@@ -1,38 +1,37 @@
-var CACHE_NAME = 'hi-reqwey-cache-202210311404';  
-var urlsToCache = [  
-    '/',
-    '/assets/images/avatar.png',
-    '/assets/images/favicon.ico',
-    '/assets/images/background.webp',
-    '/assets/css/reqwey.min.css',
-    '/assets/css/semantic.min.css',
-    '/assets/webfonts/fwzcN.OTF'
-];  
-  
-self.addEventListener('install', function (event) {  
-    event.waitUntil(  
-        caches.open(CACHE_NAME).then(function (cache) {  
-            console.log('Open cache');  
-            return cache.addAll(urlsToCache);  
-        }).then(function () {  
-            self.skipWaiting();  
-        })  
-    );  
-}); 
+var CACHE_NAME = 'hi-reqwey-cache-202210312048';
+var urlsToCache = [
+  '/',
+  '/assets/images/avatar.png',
+  '/assets/images/favicon.ico',
+  '/assets/images/background.webp',
+  '/assets/css/reqwey.min.css',
+  '/assets/css/semantic.min.css',
+  '/assets/webfonts/fwzcN.OTF'
+];
 
-self.addEventListener('fetch', function(event){  
-    if(event.request.method !== 'GET') return;  
-    event.respondWith(  
-        caches.match(event.request).then(function(response){  
-            if(response){  
-                console.log('return caches');  
-                return response;  
-            }else{  
-                return fetch(event.request).catch(function(){  
-                    if(/\.html$/.test(event.request.url))  
-                        return caches.match('/offline.html');  
-                });  
-            }  
-        })  
-    )  
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('Opening cache');
+      return cache.addAll(urlsToCache);
+    }).then(function () {
+      self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        console.log('Returned cache file ' + event.request);
+        return response;
+      } else {
+        return fetch(event.request).catch(function (e) {
+          console.log(e.message);
+        });
+      }
+    })
+  )
 });
